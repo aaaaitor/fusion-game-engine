a22a6c78e33ee47a17c5493ffc5d0cf48672de27
aaaaitor <aitortxumu@gmail.com>
2024-04-11 04:15:01 +0200

    Started project creation implementation.

---
+++ Engine/Fusion/FusionEditor/Common/BaseViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel;
+
+namespace FusionEditor
+{
+    /// <summary>
+    /// Base class for the view models.
+    /// </summary>
+    internal class BaseViewModel : INotifyPropertyChanged
+    {
+        //Event for when a property changes.
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        /// <summary>
+        /// Invokes the "PropertyChanged" event when a change in a property is detected.
+        /// </summary>
+        /// <param name="propertyName">The property name.</param>
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ Engine/Fusion/FusionEditor/GameProject/NewProject.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FusionEditor.GameProject
+{
+    /// <summary>
+    /// Class to handle the creation of a new project.
+    /// </summary>
+    class NewProject : BaseViewModel
+    {
+        private string _name = "New Project";
+        public string Name { get { return _name; } set { if (_name != value) { _name = value; } OnPropertyChanged(nameof(Name)); } }
+        private string _path = $@"{Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments)}\FusionProject\";
+        public string Path { get { return _path; } set { if (_path != value) { _path = value; OnPropertyChanged(nameof(Path)); } } }
+    }
+
+    //TODO 17:20 VIDEO
+}
