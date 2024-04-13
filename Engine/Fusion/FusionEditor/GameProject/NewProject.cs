4b21e906ff89dd49a570b507e50fe86ca5fadfbc
aaaaitor <aitortxumu@gmail.com>
2024-04-13 02:20:29 +0200

    Project creation window updated.

--- Engine/Fusion/FusionEditor/GameProject/NewProject.cs
+++ Engine/Fusion/FusionEditor/GameProject/NewProject.cs
@@ -1,21 +1,72 @@
-﻿using System;
+﻿using FusionEditor.Utilities;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace FusionEditor.GameProject
 {
+    [DataContract]
+    public class ProjectTemplate
+    {
+        [DataMember]
+        public required string ProjectType { get; set; }
+        [DataMember]
+        public required string ProjectFile { get; set; }
+        [DataMember]
+        public required List<string> ProjectFolders { get; set; }
+
+        public required byte[] Icon { get; set; }
+        public required byte[] Screenshot { get; set; }
+        public required string IconFilePath { get; set; }
+        public required string ScreenshotFilePath { get; set; }
+        public required string ProjectFilePath { get; set; }
+    }
+
     /// <summary>
     /// Class to handle the creation of a new project.
     /// </summary>
     class NewProject : BaseViewModel
     {
-        private string _name = "New Project";
-        public string Name { get { return _name; } set { if (_name != value) { _name = value; } OnPropertyChanged(nameof(Name)); } }
-        private string _path = $@"{Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments)}\FusionProject\";
-        public string Path { get { return _path; } set { if (_path != value) { _path = value; OnPropertyChanged(nameof(Path)); } } }
-    }
+        //TODO: use relative path
+        private readonly string _templatePath = @"..\..\FusionEditor\ProjectTemplates";
+        private string _projectName = "New Project";
+        public string ProjectName { get { return _projectName; } set { if (_projectName != value) { _projectName = value; } OnPropertyChanged(nameof(ProjectName)); } }
+        private string _projectPath = $@"{Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments)}\FusionProject\";
+        public string ProjectPath { get { return _projectPath; } set { if (_projectPath != value) { _projectPath = value; OnPropertyChanged(nameof(ProjectPath)); } } }
+        private ObservableCollection<ProjectTemplate> _projectTemplates = new ObservableCollection<ProjectTemplate>();
+        public ReadOnlyObservableCollection<ProjectTemplate> ProjectTemplates { get; }
 
-    //TODO 17:20 VIDEO
+        public NewProject()
+        {
+            ProjectTemplates = new ReadOnlyObservableCollection<ProjectTemplate>(_projectTemplates);
+            try
+            {
+                var templateFiles = Directory.GetFiles(_templatePath, "template.xml", SearchOption.AllDirectories);
+                Debug.Assert(templateFiles.Any());
+                foreach (var templateFile in templateFiles)
+                {
+                    var template = Serializer.FromFile<ProjectTemplate>(templateFile);
+                    template.IconFilePath = Path.GetFullPath(Path.Combine(Path.GetDirectoryName(templateFile), "Icon.png"));
+                    template.Icon = File.ReadAllBytes(template.IconFilePath);
+                    template.ScreenshotFilePath = Path.GetFullPath(Path.Combine(Path.GetDirectoryName(templateFile), "Screenshot.jpg"));
+                    template.Screenshot = File.ReadAllBytes(template.ScreenshotFilePath);
+                    template.ProjectFilePath = Path.GetFullPath(Path.Combine(Path.GetDirectoryName(templateFile), template.ProjectFile));
+
+                    _projectTemplates.Add(template);
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine($@"Error while retrieving project template files: {ex.Message}");
+                //TODO: log error
+            }
+        }
+    }
 }
---
+++ Engine/Fusion/FusionEditor/Utilities/Serializer.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Animation;
+using System.Xml.Schema;
+
+namespace FusionEditor.Utilities
+{
+	/// <summary>
+	/// Class for file serialization.
+	/// </summary>
+    public static class Serializer
+    {
+		/// <summary>
+		/// Writes the given data to the specified path.
+		/// </summary>
+		/// <typeparam name="T"></typeparam>
+		/// <param name="instance">Data instance.</param>
+		/// <param name="path">Writing path.</param>
+        public static void ToFile<T>(T instance, string path)
+        {
+			try
+			{
+				using var fs = new FileStream(path, FileMode.Create);
+				var serializer = new DataContractSerializer(typeof(T));
+				serializer.WriteObject(fs, instance);
+			}
+			catch (Exception ex)
+			{
+				Debug.WriteLine($@"Error in data serialization on file writing: {ex.Message}");
+				//TODO: log error
+			}
+        }
+
+		/// <summary>
+		/// Reads the data from a specified path.
+		/// </summary>
+		/// <typeparam name="T"></typeparam>
+		/// <param name="path">Reading path.</param>
+		/// <returns></returns>
+		public static T FromFile<T>(string path)
+		{
+            try
+            {
+                using var fs = new FileStream(path, FileMode.Open);
+                var serializer = new DataContractSerializer(typeof(T));
+                T instance = (T)serializer.ReadObject(fs);
+				Debug.Assert(instance != null);
+				
+				return instance;
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine($@"Error in data serialization on file reading: {ex.Message}");
+				//TODO: log error
+				return default;
+            }
+        }
+    }
+}
